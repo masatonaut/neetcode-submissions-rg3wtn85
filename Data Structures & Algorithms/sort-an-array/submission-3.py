e5674859b5c0bae416322b859bac7b2e3fd9de4f
Masato Ito <66577866+masatonaut@users.noreply.github.com>
2026-05-13 18:44:05 +0900

    Add: sort-an-array - submission-3

---
+++ Data Structures & Algorithms/sort-an-array/submission-3.py
@@ -0,0 +1,26 @@
+class Solution:
+    def sortArray(self, nums: List[int]) -> List[int]:
+        n = len(nums)
+
+        for i in range(n // 2 - 1, -1, -1):
+            self._heapify(nums, n, i)
+
+        for end in range(n - 1, 0, -1):
+            nums[0], nums[end] = nums[end], nums[0]
+            self._heapify(nums, end, 0)
+
+        return nums
+
+    def _heapify(self, nums, size, root):
+        largest = root
+        left = 2 * root + 1
+        right = 2 * root + 2
+
+        if left < size and nums[left] > nums[largest]:
+            largest = left
+        if right < size and nums[right] > nums[largest]:
+            largest = right
+
+        if largest != root:
+            nums[largest], nums[root] = nums[root], nums[largest]
+            self._heapify(nums, size, largest)
