10dca5010852f356597d860011a52c5fadc8e259
Masato Ito <66577866+masatonaut@users.noreply.github.com>
2026-05-19 16:33:57 +0900

    Add: best-time-to-buy-and-sell-stock-ii - submission-3

---
+++ Data Structures & Algorithms/best-time-to-buy-and-sell-stock-ii/submission-3.py
@@ -0,0 +1,9 @@
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        free = 0
+        hold = -float('inf')
+        for price in prices:
+            prev_free, prev_hold = free, hold
+            free = max(prev_free, prev_hold + price)
+            hold = max(prev_hold, prev_free - price)
+        return free
