7b19aaf02fc3873c2ddbfe87ee8d3f60f80a6311
Masato Ito <66577866+masatonaut@users.noreply.github.com>
2026-05-26 14:06:24 +0900

    Add: implement-queue-using-stacks - submission-1

---
+++ Data Structures & Algorithms/implement-queue-using-stacks/submission-1.py
@@ -0,0 +1,35 @@
+from collections import deque
+
+class MyQueue:
+
+    def __init__(self):
+        self.in_stack = []
+        self.out_stack = []
+
+    def push(self, x: int) -> None:
+        self.in_stack.append(x)
+
+    def pop(self) -> int:
+        self._move_if_needed()
+        return self.out_stack.pop()
+
+    def peek(self) -> int:
+        self._move_if_needed()
+        return self.out_stack[-1]
+
+    def empty(self) -> bool:
+        return len(self.in_stack) == 0 and len(self.out_stack) == 0
+
+    def _move_if_needed(self):
+        if not self.out_stack:
+            while self.in_stack:
+                self.out_stack.append(self.in_stack.pop())
+        
+
+
+# Your MyQueue object will be instantiated and called as such:
+# obj = MyQueue()
+# obj.push(x)
+# param_2 = obj.pop()
+# param_3 = obj.peek()
+# param_4 = obj.empty()
