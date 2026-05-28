c2ab754fb2217e27b54c37ab53560e70f30dad4f
Masato Ito <66577866+masatonaut@users.noreply.github.com>
2026-05-28 18:25:06 +0900

    Add: capacity-to-ship-packages-within-d-days - submission-1

---
+++ Data Structures & Algorithms/capacity-to-ship-packages-within-d-days/submission-1.py
@@ -0,0 +1,25 @@
+class Solution:
+    def shipWithinDays(self, weights: List[int], days: int) -> int:
+        left = max(weights)
+        right = sum(weights)
+
+        def days_needed(capacity):
+            d = 1
+            current = 0
+            for w in weights:
+                if w + current > capacity:
+                    d += 1
+                    current = w
+                else:
+                    current += w
+
+            return d
+
+        while left < right:
+            mid = (left + right) // 2
+            if days_needed(mid) <= days:
+                right = mid
+            else:
+                left = mid + 1
+
+        return left
