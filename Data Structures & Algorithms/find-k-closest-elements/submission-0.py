b463862a6979f2a36b97d569228e34cf4542104a
Masato Ito <66577866+masatonaut@users.noreply.github.com>
2026-06-09 13:13:31 +0900

    Add: find-k-closest-elements - submission-0

---
+++ Data Structures & Algorithms/find-k-closest-elements/submission-0.py
@@ -0,0 +1,7 @@
+class Solution:
+    def findClosestElements(self, arr: List[int], k: int, x: int) -> List[int]:
+        arr_sorted = sorted(arr, key = lambda a: (abs(a - x), a))
+        
+        closest = arr_sorted[:k]
+        
+        return sorted(closest)
