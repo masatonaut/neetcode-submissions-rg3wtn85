9fddd7a10b55f82a517b4f4057a6f74da118b0dc
Masato Ito <66577866+masatonaut@users.noreply.github.com>
2026-07-01 18:49:59 +0900

    Add: sum-of-all-subset-xor-totals - submission-1

---
+++ Data Structures & Algorithms/sum-of-all-subset-xor-totals/submission-1.py
@@ -0,0 +1,12 @@
+class Solution:
+    def subsetXORSum(self, nums: List[int]) -> int:
+        def backtrack(i, current):
+            if i == len(nums):
+                return current
+            
+            include = backtrack(i + 1, current ^ nums[i])
+            exclude = backtrack(i + 1, current)
+
+            return include + exclude
+
+        return backtrack(0, 0)
