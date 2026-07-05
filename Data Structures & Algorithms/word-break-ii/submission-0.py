31d65a0bfdc3280fcf32fb5b1dd4635297f113d4
Masato Ito <66577866+masatonaut@users.noreply.github.com>
2026-07-05 17:11:29 +0900

    Add: word-break-ii - submission-0

---
+++ Data Structures & Algorithms/word-break-ii/submission-0.py
@@ -0,0 +1,19 @@
+class Solution:
+    def wordBreak(self, s: str, wordDict: List[str]) -> List[str]:
+        wordSet = set(wordDict)
+        result = []
+
+        def backtrack(start, path):
+            if start == len(s):
+                result.append(" ".join(path))
+                return
+
+            for end in range(start + 1, len(s) + 1):
+                word = s[start:end]
+                if word in wordSet:
+                    path.append(word)
+                    backtrack(end, path)
+                    path.pop()
+
+        backtrack(0, [])
+        return result
