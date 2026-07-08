81a1874efe65f95597243c566eaab9196c2ae8c2
Masato Ito <66577866+masatonaut@users.noreply.github.com>
2026-07-08 16:43:53 +0900

    Add: accounts-merge - submission-2

---
+++ Data Structures & Algorithms/accounts-merge/submission-2.py
@@ -0,0 +1,32 @@
+from collections import defaultdict
+
+class Solution:
+    def accountsMerge(self, accounts: List[List[str]]) -> List[List[str]]:
+        graph = defaultdict(set)
+        email_to_name = {}
+
+        for account in accounts:
+            name = account[0]
+            first = account[1]
+            for email in account[1:]:
+                email_to_name[email] = name
+                graph[first].add(email)
+                graph[email].add(first)
+
+        visited = set()
+        def dfs(email, group):
+            visited.add(email)
+            group.append(email)
+            for nei in graph[email]:
+                if nei not in visited:
+                    dfs(nei, group)
+
+        result = []
+        for email in email_to_name:
+            if email not in visited:
+                group = []
+                dfs(email, group)
+                name = email_to_name[email]
+                result.append([name] + sorted(group))
+
+        return result
