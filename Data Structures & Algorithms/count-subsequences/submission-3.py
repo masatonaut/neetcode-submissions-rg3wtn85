6453d41156a4b58b5973177f065ee978309cfc6c
Masato Ito <66577866+masatonaut@users.noreply.github.com>
2026-07-21 23:23:07 +0900

    Add: count-subsequences - submission-3

---
+++ Data Structures & Algorithms/count-subsequences/submission-3.py
@@ -0,0 +1,19 @@
+from functools import lru_cache
+
+class Solution:
+    def numDistinct(self, s: str, t: str) -> int:
+        m, n = len(s), len(t)
+
+        @lru_cache(maxsize=None)
+        def dfs(i, j):
+            if j == n:
+                return 1
+            if i == m:
+                return 0
+
+            count = dfs(i + 1, j)
+            if s[i] == t[j]:
+                count += dfs(i + 1, j + 1)
+            return count
+
+        return dfs(0, 0)
